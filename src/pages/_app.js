8a5ae5f429ddbbef83d128a446ad066546a52624
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-03-22 22:58:50 -0500

    terminado la navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,7 @@
 import "@/styles/globals.css";
-import Navbar from "@/components/Navbar";
 
 const App = ({ Component, pageProps, router }) => {
-  return (
-    <>
-      <Navbar />
-      <div className="pt-[80px]">
-        <Component key={router.pathname} {...pageProps} />
-      </div>
-    </>
-  );
+  return <Component key={router.pathname} {...pageProps} />;
 };
 
 export default App;
